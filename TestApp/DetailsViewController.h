4e4c9f1ebd153a2d411685e6bdc4e8b29dfa77c3
Andrey Kalinin <andreykalinin@Mac-mini-Vadim.local>
2017-06-24 17:53:45 +0300

    add missed details screen

---
+++ TestApp/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  TestApp
+//
+//  Created by Andrey Kalinin on 6/24/17.
+//  Copyright © 2017 Andrey Kalinin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
